cf72eb4a4ad8f507c98141459b666e9fd26a1877
robertlowder <rlowder@confederationcollege.ca>
2025-02-21 22:27:40 -0500

    Step 3: Write a test that asserts that when generate_prime_factors is called with 2, the list [2] is returned. Solve & commit.

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,18 +1,24 @@
 # tests/test_generate_prime_factors.py
 import pytest
 import prime
-from prime import generate_prime_factors
-
 
 def test_not_integer():
     # check if string
     with pytest.raises(ValueError):
-       prime.generate_prime_factors("string")
+        prime.generate_prime_factors("string")
 
    # check if float
     with pytest.raises(ValueError):
         prime.generate_prime_factors(6.66)
 
+
 def test_input_1():
-    # check that an empty list is returned if a 1 is entered
-    assert generate_prime_factors(1) == []
+    # check that an empty list is returned
+    # generate_prime_factors is called with a 1
+    assert prime.generate_prime_factors(1) == []
+
+
+def test_input_2():
+    # check that the list [2] is returned if
+    # generate_prime_factors is called with a 2
+    assert prime.generate_prime_factors(2) == [2]
--- prime.py
+++ prime.py
@@ -2,7 +2,6 @@
 prime.py -- Write the application code here
 """
 
-
 def generate_prime_factors(num):
 
     # if function is sent anything other than an integer, raise ValueError message
@@ -12,3 +11,7 @@ def generate_prime_factors(num):
     # if function is sent a 1 return an empty List
     if num == 1:
         return []
+
+    # if function is sent a 2 return list [2]
+    if num == 2:
+        return [2]
