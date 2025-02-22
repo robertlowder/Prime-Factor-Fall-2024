7c45e220c2a586eb1875a8eaca21fd5fec4cbcf3
robertlowder <rlowder@confederationcollege.ca>
2025-02-21 21:38:59 -0500

    Step 2:
    Write a test that asserts that when generate_prime_factors is called with 1, an empty list is returned. Solve & commit.

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,6 +1,8 @@
 # tests/test_generate_prime_factors.py
 import pytest
 import prime
+from prime import generate_prime_factors
+
 
 def test_not_integer():
     # check if string
@@ -10,3 +12,7 @@ def test_not_integer():
    # check if float
     with pytest.raises(ValueError):
         prime.generate_prime_factors(6.66)
+
+def test_input_1():
+    # check that an empty list is returned if a 1 is entered
+    assert generate_prime_factors(1) == []
--- prime.py
+++ prime.py
@@ -4,5 +4,11 @@ prime.py -- Write the application code here
 
 
 def generate_prime_factors(num):
+
+    # if function is sent anything other than an integer, raise ValueError message
     if type(num) != int:
         raise ValueError('Input must be an integer.')
+
+    # if function is sent a 1 return an empty List
+    if num == 1:
+        return []
